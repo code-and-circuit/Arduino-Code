60bcdaa004496716cb283f08352ff7955348b407
TheCoder3156 <wpreese@icloud.com>
2019-02-11 17:22:03 -0500

    Format this ugly code

--- Source/Driver/Driver.ino
+++ Source/Driver/Driver.ino
@@ -4,7 +4,7 @@
 #include "SoftwareSerialParityHalfDuplex.h"
 #include <Wire.h>
 
-SoftwareSerialParityHalfDuplex mySerial(10, 10); 
+SoftwareSerialParityHalfDuplex mySerial(10, 10);
 
 unsigned long tenMilliTimer = 0;
 
@@ -18,18 +18,18 @@ int curFor = 0;
 int curTurn = 0;
 int speed = 0x0c;
 
-void setup()  
+void setup()
 {
-  Wire.begin(10); 
+  Wire.begin(10);
 
-	pinMode(13, OUTPUT);
+  pinMode(13, OUTPUT);
 
-  pinMode(10,OUTPUT);
+  pinMode(10, OUTPUT);
   digitalWrite (10, LOW);  // start serial line LOW
 
   Serial.begin(115200);
   delay(1000);
-  
+
   startupChair();
 
   tenMilliTimer = millis();
@@ -37,98 +37,99 @@ void setup()
   hundredMilliTimer = millis();
 }
 
-int coms[3] = {0,0,0};
+int coms[3] = {0, 0, 0};
 union foo
-{ 
+{
   byte a;
   char b;
 };
+
 void receiveData(void)
 {
   Serial.print(millis());
   Serial.println(" receiveData");
   int comsN = -1;
-  while (Wire.available()) 
-  { 
-  	Serial.println("Got data");
-  	if (comsN == -1)
-  	{
-	    union foo bar;
-	    bar.b = Wire.read(); // receive byte as a character
-		int c = bar.b;
-	   //	Serial.println(c);
-	    if (c == 'g')
-	    {
-	    	comsN++;
-	    }
-  	}
-	else if (comsN >= 0)
-	{
-    	  union foo bar;
-	    bar.a = Wire.read(); // receive byte as a character
-//	   	Serial.println(c);
-		int c = bar.b;
-	    coms[comsN] = c; // receive byte 
-		comsN++;
-	
-		if (comsN == 3)
-		{
-			comsN = -1;
-			int a = coms[0];
-			int t = coms[1];
-			int s = coms[2];
-			/*
-				    	Serial.print(a);
-				    	Serial.print(" ");
-				    	Serial.print(t);
-				    	Serial.print(" ");
-				    	Serial.println(s);
-	*/
-			if (a >= -100 && a <= 100 && t >= -100 && t <= 100 && s >= 0 && s <= 0xe)
-			{
-				
-			//	Serial.println("Received command: ");
-		
-				Serial.print("Forward/Backward: ");
-			    Serial.print (a);         
-			
-				Serial.print(" Left/write: ");
-			    Serial.print(t);         
-			
-				Serial.print(" Speed: ");
-			    Serial.println(s);     
-			    
-		   		curFor = a;
-		    	curTurn = t;
-		    	speed = s;
-			}
-			else
-			{
-				Serial.println("Received bad command");
-				curFor = 0;
-		    	curTurn = 0;
-		    	speed = 0;
-			}
-		}
-	}
+  while (Wire.available())
+  {
+    Serial.println("Got data");
+    if (comsN == -1)
+    {
+      union foo bar;
+      bar.b = Wire.read(); // receive byte as a character
+      int c = bar.b;
+      //	Serial.println(c);
+      if (c == 'g')
+      {
+        comsN++;
+      }
+    }
+    else if (comsN >= 0)
+    {
+      union foo bar;
+      bar.a = Wire.read(); // receive byte as a character
+      //	   	Serial.println(c);
+      int c = bar.b;
+      coms[comsN] = c; // receive byte
+      comsN++;
+
+      if (comsN == 3)
+      {
+        comsN = -1;
+        int a = coms[0];
+        int t = coms[1];
+        int s = coms[2];
+        /*
+        	    	Serial.print(a);
+        	    	Serial.print(" ");
+        	    	Serial.print(t);
+        	    	Serial.print(" ");
+        	    	Serial.println(s);
+        */
+        if (a >= -100 && a <= 100 && t >= -100 && t <= 100 && s >= 0 && s <= 0xe)
+        {
+
+          //	Serial.println("Received command: ");
+
+          Serial.print("Forward/Backward: ");
+          Serial.print (a);
+
+          Serial.print(" Left/write: ");
+          Serial.print(t);
+
+          Serial.print(" Speed: ");
+          Serial.println(s);
+
+          curFor = a;
+          curTurn = t;
+          speed = s;
+        }
+        else
+        {
+          Serial.println("Received bad command");
+          curFor = 0;
+          curTurn = 0;
+          speed = 0;
+        }
+      }
+    }
   }
 }
 
 void startupChair()
 {
-	// activate base with 110 ms pulse
-  digitalWrite (10, HIGH);  
+  // activate base with 110 ms pulse
+  digitalWrite (10, HIGH);
   delay(110);
- 
-  // start up software serial with even parity   
+
+  // start up software serial with even parity
   mySerial.begin(38400, EVEN);
 
   Serial.println("Started up base");
- 
+
   // send init packet
   unsigned char buf2[2] = { 0x52, 0xad };
   mySerial.write(buf2, 2);
-  
+
   // base will respond, but we don't care about the response
   delay(10);
 }
@@ -137,68 +138,68 @@ int cycleCount = 0;
 
 void sendJoystickPacket(int forward, int turn, int speed)
 {
-	unsigned char buffer[6] = { 0x4a, 0x01, 0x03, 0x00, 0x00, 0x00 };
+  unsigned char buffer[6] = { 0x4a, 0x01, 0x03, 0x00, 0x00, 0x00 };
 
-	buffer[2] = speed;
-	buffer[3] = forward;
-	buffer[4] = turn;
-/*	Serial.print(millis());
-	Serial.print(" Sending :");
-	Serial.print(forward);
-	Serial.print(" ");
-	Serial.print(turn);
-	Serial.print(" ");
-	Serial.println(speed);
-*/
-	buffer[5] = 0xff - (buffer[0] + buffer[1] + buffer[2] + buffer[3] + buffer[4]);
+  buffer[2] = speed;
+  buffer[3] = forward;
+  buffer[4] = turn;
+  /*	Serial.print(millis());
+  	Serial.print(" Sending :");
+  	Serial.print(forward);
+  	Serial.print(" ");
+  	Serial.print(turn);
+  	Serial.print(" ");
+  	Serial.println(speed);
+  */
+  buffer[5] = 0xff - (buffer[0] + buffer[1] + buffer[2] + buffer[3] + buffer[4]);
 
-    mySerial.write(buffer, 6);
+  mySerial.write(buffer, 6);
 }
 
-void loop() 
+void loop()
 {
-	unsigned long milli = millis();
-	
-	// once per second
+  unsigned long milli = millis();
+
+  // once per second
   if (milli >= oneSecondTimer)
   {
-  
 
-     cycleCount++;
-     oneSecondTimer += 1000;		    	
+
+    cycleCount++;
+    oneSecondTimer += 1000;
 
   }
-  
-  	// every 100 ms
+
+  // every 100 ms
   if (milli >= hundredMilliTimer)
   {
-  	  	digitalWrite(13, HIGH);
+    digitalWrite(13, HIGH);
 
     Wire.requestFrom(5, 4);
     curFor = 0; // reset in case lost connection
- 	curTurn = 0;
-	receiveData();
-	  	digitalWrite(13, LOW);
+    curTurn = 0;
+    receiveData();
+    digitalWrite(13, LOW);
 
     hundredMilliTimer = millis() + 100;
 
   }
 
-	// every 10 ms
+  // every 10 ms
   if (milli >= tenMilliTimer)
   {
     tenMilliTimer += 10;
 
-  
-	
+
+
     if (cycleCount <= 1)
     {
-		// don't drive for first second, otherwise it will not work
-    	sendJoystickPacket(0, 0, speed);
+      // don't drive for first second, otherwise it will not work
+      sendJoystickPacket(0, 0, speed);
     }
     else
     {
-   		sendJoystickPacket(curFor, curTurn, speed);
+      sendJoystickPacket(curFor, curTurn, speed);
     }
   }
   // chair will respond, but we don't care
