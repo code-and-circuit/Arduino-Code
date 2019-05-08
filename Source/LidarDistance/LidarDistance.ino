0a591ef3ac9005b3a555c56a17de982bc08f449a
Will Reese <37090477+TheCoder3156@users.noreply.github.com>
2019-05-08 17:29:18 -0400

    Rewrite and format the lidar code
    We can change the license because of this.

--- Source/LidarDistance/LidarDistance.ino
+++ Source/LidarDistance/LidarDistance.ino
@@ -1,68 +1,31 @@
-/*
-Example code for Benewake TFMini time-of-flight distance sensor. 
-by Peter Jansen (December 11/2017)
-This example code is in the public domain.
-
-This example communicates to the TFMini using a SoftwareSerial port at 115200, 
-while communicating the distance results through the default Arduino hardware
-Serial debug port. 
-
-SoftwareSerial for some boards can be unreliable at high speeds (such as 115200). 
-The driver includes some limited error detection and automatic retries, that
-means it can generally work with SoftwareSerial on (for example) an UNO without
-the end-user noticing many communications glitches, as long as a constant refresh
-rate is not required. 
-
-The (UNO) circuit:
- * Uno RX is digital pin 10 (connect to TX of TF Mini)
- * Uno TX is digital pin 11 (connect to RX of TF Mini)
-
-THIS SOFTWARE IS PROVIDED ''AS IS'' AND ANY
-EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE AUTHOR(S) BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  
-*/
-
 #include <SoftwareSerial.h>
 #include "TFMini.h"
 
-// Setup software serial port 
-SoftwareSerial mySerial(10, 11);      // Uno RX (TFMINI TX), Uno TX (TFMINI RX)
-TFMini tfmini;
+SoftwareSerial lidarSerial(10, 11);
 
-void setup() {
-  // Step 1: Initialize hardware serial port (serial debug port)
+TFMini jazzyLidar;
+
+void setup()
+{
   Serial.begin(115200);
-  // wait for serial port to connect. Needed for native USB port only
-  while (!Serial);
-     
-  Serial.println ("Initializing...");
 
-  // Step 2: Initialize the data rate for the SoftwareSerial port
-  mySerial.begin(TFMINI_BAUDRATE);
+  while(!Serial); //Wait for serial
 
-  // Step 3: Initialize the TF Mini sensor
-  tfmini.begin(&mySerial);    
+  lidarSerial.begin(TFMINI_BAUDRATE); //Start serial for the lidar at the baudrate for the tfmini
+
+  tfmini.begin(&lidarSerial); //Initalize the lidar via the lidar serial port
 }
 
 
-void loop() {
-  // Take one TF Mini distance measurement
-  uint16_t dist = tfmini.getDistance();
-  uint16_t strength = tfmini.getRecentSignalStrength();
-  
-  // Display the measurement
-  Serial.print(dist);
-  Serial.print(" cm      sigstr: ");
+void loop()
+{
+  uint16_t distance = tfmini.getDistance(); //Get the distance of the nearest object in centimeters
+  uint16_t strength = tfmini.getRecentSignalStrength(); //Get the signal strength
+
+  Serial.print(distance); //Print the distance and strength
+  Serial.println(" cm");
+  Serial.print("Strength = ");
   Serial.println(strength);
 
-  // Wait some short time before taking the next measurement
-  delay(25);  
+  delay(25); //Wait before reading again
 }
-
